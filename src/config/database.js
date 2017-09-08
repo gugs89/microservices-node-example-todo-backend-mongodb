11fb4740a958d51f26a4ba027f87325bd9389873
Gustavo Apolinario <gustavoapolinario@MacBook-Pro-de-Gustavo.local>
2017-09-08 17:37:53 -0300

    First test completed

---
+++ src/config/database.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+module.exports = mongoose.connect('mongodb://localhost/mymoney', { useMongoClient: true })
+
+mongoose.Error.messages.general.required = "The attibute '{PATH}' is mandatory."
+mongoose.Error.messages.Number.min = "'{VALUE}' is lower then '{MIN}'."
+mongoose.Error.messages.Number.max = "'{VALUE}' informado é maior que o limite máximo de '{MAX}'."
+mongoose.Error.messages.String.enum = "'{VALUE}' is not valid for attribute '{PATH}'."
