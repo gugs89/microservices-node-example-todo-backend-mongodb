e281de78822e09bba953de71528c05cd1d7c2ae1
Gustavo Apolinario <gustavoapolinario@MacBook-Pro-de-Gustavo.local>
2017-09-08 17:42:49 -0300

    Updating database name

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/mymoney', { useMongoClient: true })
+module.exports = mongoose.connect('mongodb://localhost/todo', { useMongoClient: true })
 
 mongoose.Error.messages.general.required = "The attibute '{PATH}' is mandatory."
 mongoose.Error.messages.Number.min = "'{VALUE}' is lower then '{MIN}'."
