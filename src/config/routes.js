d1db577b82fb402e15114568000d4c1542020ccc
= <=>
2018-09-01 12:53:45 -0300

    Arrumando package

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,11 +2,9 @@ const express = require('express')
 
 module.exports = function(server) {
 
-    // Definir URL base para todas as rotas 
     const router = express.Router()
     server.use('/', router)
 
-    // Rotas de Ciclo de Pagamento 
     const todo = require('../api/todo/todoService')
     todo.register(router, '/todo')
 }
