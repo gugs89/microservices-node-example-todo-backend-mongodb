86f8ebfe3b9993099c8129b3df22edcb8993d7a7
= <=>
2018-12-23 17:58:49 -0200

    Health check

--- server/boot/root.js
+++ server/boot/root.js
@@ -4,5 +4,8 @@ module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
   router.get('/', server.loopback.status());
+  router.get('/health_check', function(req, res) {
+    res.send('OK');
+  });
   server.use(router);
 };
